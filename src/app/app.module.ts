808c4c3fa333aff745ceea6337bb2f90cdb34e69
Ester Lorente <ester.lorente@est.fib.upc.edu>
2017-03-04 03:39:57 +0100

    Login and Home pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { Page1 } from '../pages/page1/page1';
-import { Page2 } from '../pages/page2/page2';
+import { LoginPage } from '../pages/login/login';
+import { HomePage } from '../pages/home/home';
+import { AuthService } from '../providers/auth-service';
 
 @NgModule({
   declarations: [
     MyApp,
-    Page1,
-    Page2
+    LoginPage,
+    HomePage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -16,9 +17,9 @@ import { Page2 } from '../pages/page2/page2';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    Page1,
-    Page2
+    LoginPage,
+    HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, AuthService],
 })
 export class AppModule {}
