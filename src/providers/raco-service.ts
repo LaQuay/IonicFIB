85c018ac1db1739ba40cfcd677f73bf205d947b2
Ester Lorente <ester.lorente@est.fib.upc.edu>
2017-03-04 12:57:18 +0100

    Public access Raco API

--- src/providers/raco-service.ts
+++ src/providers/raco-service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
+var CLIENT_ID = 'lM6FOOG62LliFSfxYbD3MqwxSDUR7JuEodgNFRX8';
+
 @Injectable()
 export class RacoService {
     data = [];
@@ -21,7 +23,7 @@ export class RacoService {
             // We're using Angular HTTP provider to request the data,
             // then on the response, it'll map the JSON data to a parsed JS object.
             // Next, we process the data and resolve the promise with the new data.
-            this.http.get("https://api.fib.upc.edu/v2/assignatures/?format=json")
+            this.http.get("https://api.fib.upc.edu/v2/assignatures/?format=json" + "&client_id=" + CLIENT_ID)
             .map(res => res.json())
             .subscribe(data => {
                 this.data = data;
